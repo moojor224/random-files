ee7838f805e3635914e11811827dde1351362ac7
Jordan Moore <64162862+moojor224@users.noreply.github.com>
2024-01-24 00:03:16 -0600

    Update jstools.js

--- jstools.js
+++ jstools.js
@@ -99,3 +99,15 @@ function tabColor(color) {
     b.rel = "icon";
     document.head.append(b);
 } //set tab color
+
+function dynamicSort(property) {
+    let sortOrder = 1;
+    if (property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a, b) {
+        let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
