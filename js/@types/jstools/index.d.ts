152532ba8b96e303a395e5c5c3d1e9a72772505b
moojor224 <moojor224@gmail.com>
2024-02-14 17:00:24 -0600

    work on jstools typdefs

--- js/@types/jstools/index.d.ts
+++ js/@types/jstools/index.d.ts
@@ -10,6 +10,55 @@ type Without<T, K> = {
 // @ts-ignore
 type CreateElementOptions = {} & Element;
 
+type SettingsConfig = {
+    name?: String
+}
+
+type SectionConfig = {
+    name?: String;
+    id: String;
+}
+
+type OptionConfig = {
+    name?: String;
+    id: String;
+    type: keyof OptionTypes;
+}
+
+type OptionTypes = {
+    dropdown: HTMLSelectElement;
+    slider: HTMLInputElement;
+}
+
+
+declare class Settings extends EventTarget {
+    constructor(config: SettingsConfig);
+    config: SettingsConfig;
+    sections: Section[];
+    render(): HTMLDivElement;
+    getSection(id: string): Section;
+    export(): String;
+    dispatchEvent(event: Event): boolean;
+    on(type: String, callback: Function): void;
+    off(type: String, callback: Function): void;
+    static loadJson(jsontext: String): Settings;
+    replaceWith(settings: Settings): void;
+}
+
+declare class Section {
+    constructor(config: SectionConfig);
+    config: SectionConfig;
+    settings_obj: Settings;
+    options: Options[];
+    render(): HTMLDivElement;
+}
+
+declare class Options {
+    constructor(config: OptionConfig);
+    config: OptionConfig;
+    render(): OptionTypes[typeof this.config.type];
+}
+
 interface Window {
     /**
      * creates an element
@@ -17,7 +66,11 @@ interface Window {
      * @param options properties to set
      */
     createElement<Tag extends keyof HTMLElementTagNameMap>(tagName: Tag, options?: CreateElementOptions): HTMLElementTagNameMap[Tag];
+    Settings(): Settings;
+    Section: Section;
+    Options(): Options;
 }
+
 
 interface HTMLElementTagNameMap {
     "warn": HTMLDivElement
