14223a4eeca2eaf9983547ce69debd20b4f8f132
moojor224 <moojor224@gmail.com>
2024-03-20 16:54:06 -0500

    advanced dynamic sort

--- js/jstools.js
+++ js/jstools.js
@@ -366,8 +366,7 @@ export function parseCookies(cookies = document.cookie) {
  * generates a array sort function that sorts an array of objects by a specified property name
  * @param {string} prop name of the property to sort by
  * @returns {function} the sort function
- * @example 
- * ```javascript
+ * @example
  * let People = [
  *     {Name: "Name", Surname: "Surname"},
  *     {Name:"AAA", Surname:"ZZZ"},
@@ -376,7 +375,6 @@ export function parseCookies(cookies = document.cookie) {
  * People.sort(dynamicSort("Name"));
  * People.sort(dynamicSort("Surname"));
  * People.sort(dynamicSort("-Surname"));
- * ```
  */
 export function dynamicSort(prop) {
     let sortOrder = 1; // normal sort order
@@ -387,6 +385,59 @@ export function dynamicSort(prop) {
     return function (a, b) { // use this function in array.sort(func); to sort it by the given  property name
         let result = a[prop] < b[prop] ? -1 : a[prop] > b[prop] ? 1 : 0;  // run comparison and set result to -1,0,1
         return result * sortOrder; // if sortOrder is reversed, this will return 1,0,-1
+    };
+}
+
+/**
+ * generates an array sort function that can sort by object properties with optional fallback properties if two values have the same property value\
+ * each given property can also have period-separated nested property names for advanced sorting\
+ * adding a minus before any property name will reverse the sort order for that property
+ * @param  {...string} properties list of properties to sort by
+ * @returns {function} sort function that can sort an array by the specified properties
+ * @example
+ * let func = advancedDynamicSort("a", "b");
+ * [
+ *     {a: 1, b: 2},
+ *     {a: 0, b: 3},
+ *     {a: 1: b: 1},
+ *     {a: 0, b: 1},
+ * ].sort(func);
+ * result = [
+ *     {a: 0, b: 1},
+ *     {a: 0, b: 3},
+ *     {a: 1: b: 1},
+ *     {a: 1, b: 2},
+ * ];
+ */
+export function advancedDynamicSort(...properties) {
+    // let w = (d, b) => d() ? (b(), w(d, b)) : 0;
+    function dSort(property) {
+        property = property.split(".");
+        function compare(a, b, chain) {
+            let p = chain[0].trim(), // remove whitespace from property name
+                sortOrder = 1; // ascending sort order
+            if (p[0] == "-") { // reverse the sort order
+                sortOrder = -1; // descending sort order
+                p = p.substring(1); // remove the minus sign
+            }
+            // if anything is undefined, or no more properties left in the chain
+            if (a[p] === undefined || b[p] === undefined || chain.length == 1) return sortOrder * (a[p] < b[p] ? -1 : a[p] > b[p] ? 1 : 0);
+            // if there are more than one properties left in the chain
+            if (chain.length > 1) return compare(a[p], b[p], chain.slice(1));
+        }
+        return (a, b) => compare(a, b, property); // return callable compare function
+    }
+    properties = properties.map(e => dSort(e)); // pre-generate top-level compare functions for increased sorting performance
+    return function (a, b) { // return callable sort function
+        let funcs = [...properties], // get copy of compare functions array
+            result; // stores compare function result (-1,0,1)
+        // result = funcs.shift()(a, b);
+        // w(_ => result == 0 && funcs.length > 0, _ => result = funcs.shift()(a, b));
+        do { // loop through each comparison function until one object is found to be different
+            result = funcs.shift()(a, b); // get the next available comparison function and call it with both objects
+        }
+        while (result == 0 && funcs.length > 0);
+        return result;
     };
 }
 
@@ -487,36 +538,21 @@ export function interleaveArrays(fill, ...arrays) {
 export function captureConsole() {
     if (console.everything === undefined) {
         console.everything = [];
-        function TS() { // timestamp function
-            return (new Date).toLocaleString("sv", { timeZone: 'UTC' }) + "Z"
-        }
+        let TS = _ => new Date().toLocaleString("sv", { timeZone: 'UTC' }) + "Z"; // timestamp function
         window.onerror = function (error, url, line) { // catches all console errors, includes those not made by console.error
-            console.everything.push({
-                type: "exception",
-                timeStamp: TS(),
-                value: { error, url, line }
-            })
+            console.everything.push({ type: "exception", timeStamp: TS(), value: { error, url, line } });
             return false;
         }
         window.onunhandledrejection = function (e) { // catch some other things, idk
-            console.everything.push({
-                type: "promiseRejection",
-                timeStamp: TS(),
-                value: e.reason
-            });
+            console.everything.push({ type: "promiseRejection", timeStamp: TS(), value: e.reason });
         }
         function hookLogType(logType) {
             const original = console[logType].bind(console); // save orginal function
             return function (...args) {
-                console.everything.push({ // add object to console.everything
-                    type: logType,
-                    timeStamp: TS(),
-                    value: Array.from(args)
-                });
+                console.everything.push({ type: logType, timeStamp: TS(), value: Array.from(args) }); // add object to console.everything
                 original.apply(console, args); // log message to console
             }
         }
-
         ['log', 'error', 'warn', 'debug'].forEach(logType => { // hook  each log type
             console[logType] = hookLogType(logType)
         });
