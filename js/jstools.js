1c51749356f63504030caba1508f4a83fb583a39
moojor224 <moojor224@gmail.com>
2024-05-15 16:37:49 -0500

    add custom elements to jstools

--- js/jstools.js
+++ js/jstools.js
@@ -255,7 +255,15 @@ export function clearError(...selectors) {
  * @param  {...(String|Element)} selectors list of css selectors or elements
  */
 export function hide(...selectors) {
-    for (let s of selectors) (typeof s == "string" ? document.querySelector(s) : s).classList.add("hidden"); // loop through given elements/selectors and add the hidden class
+    for (let s of selectors) {
+        let el;
+        if (typeof s == "string") {
+            el = [...document.querySelectorAll(s)];
+        } else {
+            el = [s];
+        }
+        el.forEach(e => e.classList.add("hidden")); // loop through given elements/selectors and add the hidden class
+    }
 }
 
 /**
@@ -263,7 +271,15 @@ export function hide(...selectors) {
  * @param  {...(String|Element)} selectors list of css selectors or elements
  */
 export function show(...selectors) {
-    for (let s of selectors) (typeof s == "string" ? document.querySelector(s) : s).classList.remove("hidden"); // loop through given elements/selectors and remove the hidden class
+    for (let s of selectors) {
+        let el;
+        if (typeof s == "string") {
+            el = [...document.querySelectorAll(s)];
+        } else {
+            el = [s];
+        }
+        el.forEach(e => e.classList.remove("hidden")); // loop through given elements/selectors and remove the hidden class
+    }
 }
 
 /**
@@ -2317,4 +2333,144 @@ const polyfills = (function () {
             return window.getComputedStyle(this);
         }
     }
+})();
+
+/**
+ * defines some custom elements
+ */
+export const CUSTOM_ELEMENTS = (function () {
+    function slider() { // input toggle slider
+        customElements.define("input-slider", class extends HTMLElement {
+            static observedAttributes = ["checked"];
+            #checked = this.getAttribute("checked") == "true";
+            constructor() {
+                super();
+                this.attachShadow({ mode: "open" });
+                const CSS = /*css*/`
+                    :host {
+                        --scale: 1;
+                        --duration: 0.25s;
+    
+                        --outerline-on: #0f0;
+                        --outerline-off: #f00;
+    
+                        --innerline-on: #0f0;
+                        --innerline-off: #f00;
+    
+                        --inner-shade-on: #0f0;
+                        --outer-shade-on: #fff;
+    
+                        --inner-shade-off: #f00;
+                        --outer-shade-off: #fff;
+    
+                        --show-text: 1;
+                        --off-text: "OFF";
+                        --on-text: "ON";
+    
+                        display: inline-block;
+                        user-select: none;
+                    }
+                    
+                    div.slider {
+                        margin: 0px;
+                        position: relative;
+                        width: calc(48px * var(--scale));
+                        height: calc(28px * var(--scale));
+                        display: block;
+                        border: calc(2px * var(--scale)) solid var(--outerline-off);
+                        box-sizing: border-box;
+                        border-radius: calc(14px * var(--scale));
+                        transition-duration: var(--duration);
+                        background-color: var(--outer-shade-off);
+                        transition-property: background-color, border-color;
+                        overflow: hidden;
+                    }
+                    
+                    .slider div.dot {
+                        position: absolute;
+                        /* box-sizing: border-box; */
+                        border-radius: calc(10px * var(--scale));
+                        width: calc(16px * var(--scale));
+                        height: calc(16px * var(--scale));
+                        top: calc(2px * var(--scale));
+                        left: calc(2px * var(--scale));
+                        border: calc(2px * var(--scale)) solid var(--innerline-off);
+                        background-color: var(--inner-shade-off);
+                        transition-property: left, right, background-color, border-color;
+                        transition-duration: var(--duration);
+                    }
+                    
+                    .slider[checked="true"] {
+                        border-color: var(--outerline-on);
+                        background-color: var(--outer-shade-on);
+                    }
+    
+                    .slider[checked="true"] div.dot {
+                        left: calc(22px * var(--scale));
+                        border-color: var(--innerline-on);
+                        background-color: var(--inner-shade-on);
+                    }
+    
+                    .slider div.off-text,
+                    .slider div.on-text{
+                        display: flex;
+                        height: 100%;
+                        align-items: center;
+                        position: absolute;
+                        font-size: calc(10px * var(--scale) * var(--show-text) / var(--show-text));
+                    }
+    
+                    .slider div.off-text span::before {
+                        display: block;
+                        content: var(--off-text);
+                    }
+    
+                    .slider div.on-text span::before {
+                        display: block;
+                        content: var(--on-text);
+                    }
+    
+                    .slider div.on-text {
+                        /* left: calc(2px * var(--scale)); */
+                        right: calc(4px * var(--scale) + 100%);
+                    }
+    
+                    .slider div.off-text {
+                        /* right: calc(2px * var(--scale)); */
+                        left: calc(4px * var(--scale) + 100%);
+                    }
+                `;
+                this.shadowRoot.innerHTML = /*html*/`
+                <style>${CSS}</style>
+                <div class="slider" checked="${this.#checked}">
+                    <div class="dot">
+                        <div class="on-text"><span></span></div>
+                        <div class="off-text"><span></span></div>
+                    </div>
+                </div>`;
+                this.addEventListener("click", () => {
+                    let newChecked = !(this.shadowRoot.querySelector(".slider").getAttribute("checked") == "true");
+                    this.shadowRoot.querySelector(".slider").setAttribute("checked", newChecked);
+                    this.checked = newChecked;
+                });
+            }
+            attributeChangedCallback(name, oldValue, newValue) {
+                if (name == "checked") {
+                    this.#checked = newValue.toString() == "true" && !!newValue;
+                }
+            }
+            set checked(value) {
+                this.dispatchEvent(new Event("change"));
+                this.setAttribute("checked", value);
+                this.#checked = !!value;
+            }
+            get checked() {
+                return this.#checked;
+            }
+        });
+    }
+    function all() {
+        slider();
+    }
+    return { slider, all };
 })();
