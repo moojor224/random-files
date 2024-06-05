0343b71673b939a302aa97f8ec4b0acebf24a0b1
moojor224 <moojor224@gmail.com>
2024-06-05 17:03:07 -0500

    commit

--- js/synced/jstools.js
+++ js/synced/jstools.js
@@ -2525,5 +2525,217 @@ export class jst_CSSStyleSheet {
         }
         return this.rules.map(e => e.compile()).join("\n");
     }
-
 }
+
+const CSSColors = {
+    aliceblue: "#f0f8ff", antiquewhite: "#faebd7", aqua: "#00ffff", aquamarine: "#7fffd4", azure: "#f0ffff",
+    beige: "#f5f5dc", bisque: "#ffe4c4", black: "#000000", blanchedalmond: "#ffebcd", blue: "#0000ff",
+    blueviolet: "#8a2be2", brown: "#a52a2a", burlywood: "#deb887", cadetblue: "#5f9ea0", chartreuse: "#7fff00",
+    chocolate: "#d2691e", coral: "#ff7f50", cornflowerblue: "#6495ed", cornsilk: "#fff8dc", crimson: "#dc143c",
+    cyan: "#00ffff", darkblue: "#00008b", darkcyan: "#008b8b", darkgoldenrod: "#b8860b", darkgray: "#a9a9a9",
+    darkgreen: "#006400", darkgrey: "#a9a9a9", darkkhaki: "#bdb76b", darkmagenta: "#8b008b", darkolivegreen: "#556b2f",
+    darkorange: "#ff8c00", darkorchid: "#9932cc", darkred: "#8b0000", darksalmon: "#e9967a", darkseagreen: "#8fbc8f",
+    darkslateblue: "#483d8b", darkslategray: "#2f4f4f", darkslategrey: "#2f4f4f", darkturquoise: "#00ced1",
+    darkviolet: "#9400d3", deeppink: "#ff1493", deepskyblue: "#00bfff", dimgray: "#696969", dimgrey: "#696969",
+    dodgerblue: "#1e90ff", firebrick: "#b22222", floralwhite: "#fffaf0", forestgreen: "#228b22", fuchsia: "#ff00ff",
+    gainsboro: "#dcdcdc", ghostwhite: "#f8f8ff", gold: "#ffd700", goldenrod: "#daa520", gray: "#808080",
+    green: "#008000", greenyellow: "#adff2f", grey: "#808080", honeydew: "#f0fff0", hotpink: "#ff69b4",
+    indianred: "#cd5c5c", indigo: "#4b0082", ivory: "#fffff0", khaki: "#f0e68c", lavender: "#e6e6fa",
+    lavenderblush: "#fff0f5", lawngreen: "#7cfc00", lemonchiffon: "#fffacd", lightblue: "#add8e6", lightcoral: "#f08080",
+    lightcyan: "#e0ffff", lightgoldenrodyellow: "#fafad2", lightgray: "#d3d3d3", lightgreen: "#90ee90",
+    lightgrey: "#d3d3d3", lightpink: "#ffb6c1", lightsalmon: "#ffa07a", lightseagreen: "#20b2aa", lightskyblue: "#87cefa",
+    lightslategray: "#778899", lightsteelblue: "#b0c4de", lightyellow: "#ffffe0", lime: "#00ff00", limegreen: "#32cd32",
+    linen: "#faf0e6", magenta: "#ff00ff", maroon: "#800000", mediumaquamarine: "#66cdaa", mediumblue: "#0000cd",
+    mediumorchid: "#ba55d3", mediumpurple: "#9370db", mediumseagreen: "#3cb371", mediumslateblue: "#7b68ee",
+    mediumspringgreen: "#00fa9a", mediumturquoise: "#48d1cc", mediumvioletred: "#c71585", midnightblue: "#191970",
+    mintcream: "#f5fffa", mistyrose: "#ffe4e1", moccasin: "#ffe4b5", navajowhite: "#ffdead", navy: "#000080",
+    oldlace: "#fdf5e6", olive: "#808000", olivedrab: "#6b8e23", orange: "#ffa500", orangered: "#ff4500",
+    orchid: "#da70d6", palegoldenrod: "#eee8aa", palegreen: "#98fb98", paleturquoise: "#afeeee", palevioletred: "#db7093",
+    papayawhip: "#ffefd5", peachpuff: "#ffdab9", peru: "#cd853f", pink: "#ffc0cb", plum: "#dda0dd", powderblue: "#b0e0e6",
+    purple: "#800080", red: "#ff0000", rosybrown: "#bc8f8f", royalblue: "#4169e1", saddlebrown: "#8b4513",
+    salmon: "#fa8072", sandybrown: "#f4a460", seagreen: "#2e8b57", seashell: "#fff5ee", sienna: "#a0522d",
+    silver: "#c0c0c0", silver: "#C0C0C0", skyblue: "#87ceeb", slateblue: "#6a5acd", slategray: "#708090",
+    slategrey: "#708090", snow: "#fffafa", springgreen: "#00ff7f", steelblue: "#4682b4", tan: "#d2b48c", teal: "#008080",
+    thistle: "#d8bfd8", tomato: "#ff6347", turquoise: "#40e0d0", violet: "#ee82ee", wheat: "#f5deb3", white: "#ffffff",
+    whitesmoke: "#f5f5f5", yellow: "#ffff00", yellowgreen: "#9acd32"
+};
+
+export class Color {
+    static hexRegex = /^#([0-9a-f]{3}){1,2}$/i;
+    static hexaRegex = /^#([0-9a-f]{4}){1,2}$/i;
+    static rgbRegex = /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/;
+    static rgbaRegex = /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d?\.?\d+)\)$/;
+    static hslRegex = /^hsl\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%\)$/;
+    static hslaRegex = /^hsla\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%,\s*(\d?\.?\d+)\)$/;
+    static hslToRgb(h, s, l) {
+        let r, g, b;
+        if (s === 0) {
+            r = g = b = l; // achromatic
+        } else {
+            const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            const p = 2 * l - q;
+            r = Color.hueToRgb(p, q, h + 1 / 3);
+            g = Color.hueToRgb(p, q, h);
+            b = Color.hueToRgb(p, q, h - 1 / 3);
+        }
+        return [Math.min(Math.floor(r * 256), 255), Math.min(Math.floor(g * 256), 255), Math.min(Math.floor(b * 256), 255)];
+    }
+    static rgbToHsl(r, g, b) {
+        (r /= 255), (g /= 255), (b /= 255);
+        const vmax = Math.max(r, g, b), vmin = Math.min(r, g, b);
+        let h, s, l = (vmax + vmin) / 2;
+        if (vmax === vmin) {
+            return [0, 0, l]; // achromatic
+        }
+        const d = vmax - vmin;
+        s = l > 0.5 ? d / (2 - vmax - vmin) : d / (vmax + vmin);
+        if (vmax === r) h = (g - b) / d + (g < b ? 6 : 0);
+        if (vmax === g) h = (b - r) / d + 2;
+        if (vmax === b) h = (r - g) / d + 4;
+        h /= 6;
+        return [h, s, l];
+    }
+    static hueToRgb(p, q, t) {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+    }
+
+    constructor(color) {
+        if (color.match(Color.hexRegex)) {
+            this.a = 1;
+            if (color.length == 4) {
+                this.r = parseInt(color[1] + color[1], 16);
+                this.g = parseInt(color[2] + color[2], 16);
+                this.b = parseInt(color[3] + color[3], 16);
+            } else {
+                this.r = parseInt(color[1] + color[2], 16);
+                this.g = parseInt(color[3] + color[4], 16);
+                this.b = parseInt(color[5] + color[6], 16);
+            }
+        } else if (color.match(Color.hexaRegex)) {
+            if (color.length == 5) {
+                this.r = parseInt(color[1] + color[1], 16);
+                this.g = parseInt(color[2] + color[2], 16);
+                this.b = parseInt(color[3] + color[3], 16);
+                this.a = parseInt(color[4] + color[4], 16);
+            } else {
+                this.r = parseInt(color[1] + color[2], 16);
+                this.g = parseInt(color[3] + color[4], 16);
+                this.b = parseInt(color[5] + color[6], 16);
+                this.a = parseInt(color[7] + color[8], 16);
+            }
+        } else if (color.match(Color.rgbRegex)) {
+            let match = color.match(Color.rgbRegex);
+            console.log("rgb match", match);
+            this.r = parseInt(match[1]);
+            this.g = parseInt(match[2]);
+            this.b = parseInt(match[3]);
+            this.a = 1;
+        } else if (color.match(Color.rgbaRegex)) {
+            let match = color.match(Color.rgbaRegex);
+            this.r = parseInt(match[1]);
+            this.g = parseInt(match[2]);
+            this.b = parseInt(match[3]);
+            this.a = parseFloat(match[4]);
+        } else if (color.match(Color.hslRegex)) {
+            let match = color.match(Color.hslRegex);
+            let h = parseInt(match[1]);
+            let s = parseInt(match[2]);
+            let l = parseInt(match[3]);
+            let [r, g, b] = Color.hslToRgb(h, s, l);
+            extend(this, { r, g, b });
+            this.a = 1;
+        } else if (color.match(Color.hslaRegex)) {
+            let match = color.match(Color.hslaRegex);
+            let h = parseInt(match[1]);
+            let s = parseInt(match[2]);
+            let l = parseInt(match[3]);
+            let [r, g, b] = Color.hslToRgb(h, s, l);
+            extend(this, { r, g, b });
+            this.a = parseFloat(match[4]);
+        } else if (CSSColors[color]) {
+            let hex = CSSColors[color];
+            this.r = parseInt(hex.substring(1, 3), 16);
+            this.g = parseInt(hex.substring(3, 5), 16);
+            this.b = parseInt(hex.substring(5, 7), 16);
+            this.a = 1;
+        } else {
+            console.error("Invalid color:", color);
+        }
+    }
+    toHex() { return `#${this.r.toString(16).padStart(2, "0")}${this.g.toString(16).padStart(2, "0")}${this.b.toString(16).padStart(2, "0")}`; }
+    toHexa() { return `#${this.r.toString(16).padStart(2, "0")}${this.g.toString(16).padStart(2, "0")}${this.b.toString(16).padStart(2, "0")}${this.a.toString(16).padStart(2, "0")}`; }
+    toRGB() { return `rgb(${this.r}, ${this.g}, ${this.b})`; }
+    toRGBA() { return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`; }
+    toHSL() {
+        let [h, s, l] = Color.rgbToHsl(this.r, this.g, this.b);
+        return `hsl(${h}, ${s}%, ${l}%)`;
+    }
+    toHSLA() {
+        let [h, s, l] = Color.rgbToHsl(this.r, this.g, this.b);
+        return `hsla(${h}, ${s}%, ${l}%, ${this.a})`;
+    }
+}
+
+export const BULK_OPERATIONS = (function () {
+    class Numbers {
+        constructor(...nums) {
+            this.nums = nums;
+        }
+    }
+    let ops = [
+        ["divide", (a, b) => a / b],
+        ["multiply", (a, b) => a * b],
+        ["add", (a, b) => a + b],
+        ["subtract", (a, b) => a - b],
+        ["pow", (a, b) => a ** b],
+        ["mod", (a, b) => a % b],
+    ];
+    for (let [name, func] of ops) {
+        Numbers.prototype[name] = function (val) {
+            return new Numbers(...this.nums.map(e => func(e, val)));
+        };
+    }
+    class Booleans {
+        constructor(...bools) {
+            this.bools = bools;
+        }
+        flip() {
+            return new Booleans(...this.bools.map(e => !e));
+        }
+    }
+    class Strings {
+        constructor(...strs) {
+            this.strs = strs;
+        }
+        // Object.getOwnPropertyNames(String.prototype).sort()
+    }
+    function gettypes(...args) {
+        let types = args.map(e => typeof e);
+        let unique = [...new Set(types)];
+        if (unique.length == 1) {
+            return unique[0];
+        }
+        return "mixed";
+    }
+    function autodetectClass(type) {
+        switch (type) {
+            case "number": return Numbers;
+            case "string": return Strings;
+            case "boolean": return Booleans;
+            case "object": return Objects;
+            case "function": return Functions;
+            default: return null;
+        }
+    }
+    return {
+        Numbers, autodetect: function (...args) {
+            let type = gettypes(...args);
+            return autodetectClass(type);
+        }
+    };
+})();
