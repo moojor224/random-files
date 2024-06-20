abe98ebe5ea1c09ac0f8fb5fc39f686001b88220
moojor224 <moojor224@gmail.com>
2024-06-20 16:36:04 -0500

    continue working on custom console

--- js/custom_console/index.js
+++ js/custom_console/index.js
@@ -1,4 +1,4 @@
-import { extend } from "../synced/jstools.js";
+import { createElement, extend, jst_CSSRule as CSSRule, jst_CSSStyleSheet as CSSStyleSheet } from "../synced/jstools.js";
 
 const escapeMap = {
     "c": "", // custom css
@@ -12,7 +12,7 @@ const escapeMap = {
 }
 /*
 background color
-log: rgb(35, 35, 39)
+
 warn: rgb(66, 56, 31)
 error: rgb(75, 47, 54)
 
@@ -26,7 +26,7 @@ error: rgb(116, 62, 76)
 vertical indent bar: object blue
 
 text color
-default: rgb(215, 215, 219)
+
 group title: bold rgb(177, 177, 179)
 object blue: rgb(117, 191, 255)
 string pink: rgb(255, 125, 233)
@@ -36,19 +36,114 @@ undefined gray: rgb(147, 147, 149)
 empty strings are replaced by <empty string> in italics
 functions: function  <name>()
 */
+let styleSheet = new CSSStyleSheet();
+let mainContainerRule = new CSSRule(".custom-console-container", {
+    display: "grid",
+    gridTemplateColumns: "1fr",
+    gridTemplateRows: "30px 1fr",
+    fontFamily: "monospace",
+    fontSize: "16px",
+    zIndex: "9999",
+    resize: "both",
+    top: "10px",
+    left: "10px",
+    position: "absolute",
+    backgroundColor: "#232327",
+    width: "500px",
+    height: "200px",
+    overflow: "auto",
+    minHeight: "60px",
+    minwidth: "100px",
+});
+let headerRule = new CSSRule(".custom-console-header", {
+    display: "grid",
+    gridTemplateColumns: "30px 1fr",
+});
+let logsRule = new CSSRule(".custom-console-logs", {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    flexDirection: "column",
+    overflow: "auto",
+    color: "rgb(215, 215, 219)",
+    border: "1px solid rgb(215, 215, 219)",
+    width: "100%",
+    height: "100%",
+    boxSizing: "border-box",
+});
 
+styleSheet.addRules(
+    mainContainerRule,
+    headerRule,
+    logsRule,
+);
+styleSheet.inject();
+
+let styles = /*css*/`
+.custom-console-message {
+    width: 100%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    border-width: 0 0 1px 0;
+    box-sizing: border-box;
+}
+.custom-console-message>* {
+    padding: 5px
+}
+.custom-console-group-indent {
+    width: 20px;
+    height: 100%;
+    border-right: 1px solid rgb(117, 191, 255);
+}
+`;
+document.head.add(createElement("style", { innerHTML: styles }));
+function timestamp() {
+    let date = new Date();
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${(date.getMilliseconds() + "").padStart(3, "0")}`
+}
+function parseLog(arg) {
+    return "" + arg;
+}
 export class CustomConsole {
     config = {}
-    constructor(parent, config) {
-        this.parent = parent;
-        extend(this.config, config);
+    constructor(parent, config = {}) {
+        if (!styleSheet.injected)
+            extend(this.config, config);
+        this.el = createElement("div", { classList: "custom-console-logs" });
+        parent.add(createElement("div", { classList: "custom-console-container" }).add(
+            createElement("div", { classList: "custom-console-header" }),
+            this.el
+        ));
+        this.x = 10;
+        this.y = 10;
+    }
+    move(x = this.x, y = this.y) {
+        this.el.style.left = x + "px";
+        this.el.style.top = y + "px";
+    }
+    _message({ backgroundColor, color, borderColor, icon }, ...args) {
+        this.el.add(createElement("div", {
+            classList: "custom-console-message",
+            style: {
+                backgroundColor: backgroundColor,
+                color: color,
+                borderColor: borderColor,
+                borderStyle: "solid",
+            }
+        }).add(
+            createElement("div", { classList: "custom-console-timestamp", innerHTML: timestamp() }),
+            createElement("div", { classList: `custom-console-icon ${icon}` }),
+            createElement("div", { classList: "custom-console-message-contents" }).add(...args.map(parseLog)),
+        ));
     }
     log(...args) {
-        this.logLine = {
-            icon: "none",
-            backgroundColor: ""
-        }
-
+        this._message({
+            backgroundColor: "#232327",
+            color: "rgb(215, 215, 219)",
+            icon: "",
+            borderColor: "#38383d",
+        }, ...args);
     }
 }
 
@@ -59,7 +154,7 @@ const defaultFormatter = {
     header: function (obj) { // placeholder
         return ["div", {
             style: "color: blue; font-weight: bold;"
-        }, obj.constructor.name];
+        }, obj.constructor.name + "{  }"];
     },
     body: function (obj) { // placeholder
         return ["div", {
@@ -79,3 +174,8 @@ function checkCustomFormatters(obj) {
         }
     });
 }
+
+let custom_console = new CustomConsole(document.body);
+custom_console.log("Hello, world!");
+custom_console.log("Hello, world!");
+custom_console.log("Hello, world!");
