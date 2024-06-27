ea4d9fa936414c37b22501dfc3f61776626f3156
moojor224 <moojor224@gmail.com>
2024-06-27 14:07:47 -0500

    test

--- python/test.py
+++ python/test.py
@@ -1,11 +1,6 @@
-from pytools import interleaveArrays
+from pytools import interleaveArrays, pyt_CSSRule
+import re
 
-print(
-    interleaveArrays(
-        None,
-        [0, 1, 2, 3, 4, 5],
-        [10, 11, 12, 13],
-        ["a", "b", "c", "d", "e"],
-    )
-)
-
+d = {"a": {"b": "b"}}
+print(isinstance(d, dict))
+print(isinstance(d["a"], dict))
