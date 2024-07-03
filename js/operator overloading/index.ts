e6a3652eef764d25b3c77c750e0f2da34ee848e7
Jordan Moore <moojor224@gmail.com>
2024-07-03 10:22:30 -0500

    move createEnum to jstools

--- js/operator overloading/index.ts
+++
@@ -1,5 +0,0 @@
-let en = createEnum({
-    type: "error type",
-    b: 2
-});
-en.type;
--- js/operator overloading/types.d.ts
+++
@@ -1,7 +0,0 @@
-// declare type ENUM<KEYS> = {
-//     [K in keyof KEYS]: KEYS[K];
-// }
-// declare const createEnum: <KEYS extends Record<string, any>>(values: KEYS) => ENUM<KEYS>
-
-// type Narrow<T> = (T extends infer U ? U : never) | Extract<T, number | string | boolean | bigint | symbol | null | undefined | []> | ([T] extends [[]] ? [] : { [K in keyof T]: Narrow<T[K]> })
-declare function createEnum<E extends Record<string, string | number | object | null | undefined | boolean>>(error: Narrow<E>): E;
