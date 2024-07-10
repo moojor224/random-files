dcc4469dfb2b34d8db0980c2f46f9dc412f6be07
moojor224 <moojor224@gmail.com>
2024-07-10 12:17:21 -0500

    testing

--- python/test.py
+++ python/test.py
@@ -5,9 +5,17 @@ d = {"a": {"b": "b"}}
 print(isinstance(d, dict))
 print(isinstance(d["a"], dict))
 
+warning = S.YELLOW + S.ITALIC
+error = S.RED + S.BOLD
+
 (S.BLUE + S.BOLD - S.BLUE)("bold")
 (S.BLUE)("blue")
 (S.BLUE + S.BOLD)("bold blue", d)
 err_type = "exception"
 (S.RED + S.BOLD)("ERROR", err_type, end=":\n\t")
 (S.ITALIC)("message")
+
+with open("out.txt", mode="w") as f:
+    print("message", file=f)
+    warning("warning", file=f)
+    error("error", file=f)
