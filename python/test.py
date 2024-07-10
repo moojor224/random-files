e534f21367258b75acb43fab28effda092ff34d3
moojor224 <moojor224@gmail.com>
2024-07-10 11:47:49 -0500

    testing

--- python/test.py
+++ python/test.py
@@ -7,4 +7,7 @@ print(isinstance(d["a"], dict))
 
 (S.BLUE + S.BOLD - S.BLUE)("bold")
 (S.BLUE)("blue")
-(S.BLUE + S.BOLD)("bold blue")
+(S.BLUE + S.BOLD)("bold blue", d)
+err_type = "exception"
+(S.RED + S.BOLD)("ERROR", err_type, end=":\n\t")
+(S.ITALIC)("message")
