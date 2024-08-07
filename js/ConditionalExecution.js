57a0fe0cda9d24c2e9cdc9c0ceef9c6398c79dc7
moojor224 <moojor224@gmail.com>
2024-08-07 11:14:58 -0500

    continue working on ConditionalExecution.js

--- js/ConditionalExecution.js
+++ js/ConditionalExecution.js
@@ -9,15 +9,38 @@ import { extend } from "./synced/jstools/index.js";
  * @prop {number?} interval interval in milliseconds to check conditions
  * @prop {AutoRunWhen} when conditions to check
  */
+let data = {}; // this object is for storing any internal data used in the hows functions
+let hows = {
+    "when.elements": function (opt) {
+        if (Array.isArray(opt)) {
+            opt = opt.join(",");
+        }
+        let els = document.querySelectorAll(opt);
+        return els.length > 0 ? Array.from(els) : false;
+    },
+    "when.urlparams": function(arg){
+        let url = new URL(window.location.href);
+        let params = url.searchParams;
+        return arg(params);
+    },
+    "when.urlparams.*": function(key, arg){
+        let url = new URL(window.location.href);
+        let params = url.searchParams;
+        let vals = params.getAll(key);
+        return vals.includes(arg);
+    }
+};
 /**
  * intended for "multiplexing" a greasemonkey script based on certain conditions
  * @param {function} callback script to run
  * @param {AutoRunOptions} options conditions for script to run
  */
-function autoRun(callback, options) {
+export function autoRun(callback, options) {
     /* @type {AutoRunOptions} */
     let defaults = extend({
-        when: function () { return true; },
+        when: {
+            elements: []
+        },
         interval: 100,
     }, options);
     if (typeof defaults.when === "function") {
@@ -39,3 +62,9 @@ function autoRun(callback, options) {
         }, time);
     }
 }
+
+autoRun(function () {
+    console.log("Hello World!");
+}, {
+
+})
