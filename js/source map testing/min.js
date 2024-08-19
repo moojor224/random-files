67934748ce978ba546be3ef7813fb6c591cfc374
moojor224 <moojor224@gmail.com>
2024-08-19 13:48:24 -0500

    test encoding a source map in base64

---
+++ js/source map testing/min.js
@@ -0,0 +1,5 @@
+f('src');
+//# sourceMappingURL=data:;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWluLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiZGF0YTp0ZXh0L2phdmFzY3JpcHQ7YmFzZTY0LFkyOXVjMjlzWlM1c2IyY29KM055WXljcE93PT0iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsQUFBQSJ9
+
+// base64 decodes to this: '{"version":3,"file":"min.js","sourceRoot":"","sources":["data:text/javascript;base64,Y29uc29sZS5sb2coJ3NyYycpOw=="],"names":[],"mappings":"AAAA,AAAA"}'
+// source base
