909903e4200e2e0bf6056c3e6611e3ab2cebc55e
moojor224 <moojor224@gmail.com>
2024-09-05 15:16:29 -0500

    add file

---
+++ github meme pilot.js
@@ -0,0 +1,13 @@
+// various autocompletions by github copilot
+/*
+on march 3, 1969, the first successful test flight of the Concorde supersonic jet was completed
+                  ^
+on march 3, 2021, the first successful test flight of the Mars Helicopter Ingenuity was completed
+^
+on march 3, 1969 the united states navy established the navy fighter weapons school, better known as TOPGUN
+                            ^
+on march 3, 1969 the united states navy established an elite school for the top one percent of its pilots. its purpose was to teach the lost art of aerial combat and to insure that the handful of men who graduated were the best fighter pilots in the world. they succeeded. today, the navy calls it fighter weapons school. the flyers call it TOPGUN. 
+                                                     ^                                                                                                                                 ^
+according to all known laws of aviation, there is no way a bee should be able to fly. its wings are too small to get its fat little body off the ground. the bee, of course, flies anyway because bees don't care what humans think is impossible.
+                ^
+*/
